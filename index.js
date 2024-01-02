0f2b1fa56c358509195d6fb61e263ee409de07b1
Atulsrj <atulsinghsrj@gmail.com>
2024-01-02 21:13:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+console.log("Hi I am Atul");
+let num = 5;
+num = 55;
+console.log(num);
+// TODO 1: Write a program to add two numbers?
+// TODO 2: How to catch error & solve it?
+// TODO 3: TS configuration file
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(5, 15));
+console.log(sum(5, "15"));
---
+++ index.ts
@@ -0,0 +1,14 @@
+console.log("Hi I am Atul");
+let num: number = 5;
+num = 55;
+console.log(num);
+
+// TODO 1: Write a program to add two numbers?
+// TODO 2: How to catch error & solve it?
+// TODO 3: TS configuration file
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(sum(5, 15));
