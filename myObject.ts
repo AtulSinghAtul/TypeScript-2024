286fab6fbaa5e9e986b773ed8f4326d9a7e04294
Atulsrj <atulsinghsrj@gmail.com>
2024-01-04 11:31:47 +0530

    READONLY and optional in typescript

--- myObject.ts
+++ myObject.ts
@@ -1,15 +1,70 @@
-let myObj = {
-  name: "Atul",
-  email: "a@a.com",
+//! Bad behaviour of objects in typescript
+
+// let myObj = {
+//   name: "Atul",
+//   email: "a@a.com",
+//   isActive: true,
+// };
+
+// function createUser({ name: string, isPaid: boolean }) {}
+
+// let newUser = { name: "atul", isPaid: false, email: "a@A.com" };
+
+// createUser(newUser);
+
+// function createCourse(): { name: string; isPaid: boolean; email: string } {
+//   return { name: "atul", isPaid: false, email: "a@A.com" };
+// }
+
+//! Type Aliases in Typescript
+
+// type User = {
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+// };
+
+// type ID = number | string;
+
+// function createUser(user: User): User {
+//   return { name: "", email: "", isActive: true };
+// }
+
+// createUser({ name: "", email: "", isActive: true });
+
+//!  READONLY and optional in typescript
+
+type User = {
+  readonly _id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+  credcardDetails?: number;
+};
+
+let myUser: User = {
+  _id: "12345",
+  name: "at",
+  email: "at@t.com",
   isActive: true,
 };
 
-function createUser({ name: string, isPaid: boolean }) {}
+myUser.name = "Atul";
+// myUser._id = "9876";
 
-let newUser = { name: "atul", isPaid: false, email: "a@A.com" };
+type cardNumber = {
+  cardNumber: string;
+};
 
-createUser(newUser);
+type cardDate = {
+  cardNumber: number;
+};
 
-function createCourse(): { name: string; isPaid: boolean; email: string } {
-  return { name: "atul", isPaid: false, email: "a@A.com" };
-}
+type cardDetailes = cardNumber &
+  cardDate & {
+    cvv: number;
+  };
+
+//! Array in Typescript
+
+export {};
