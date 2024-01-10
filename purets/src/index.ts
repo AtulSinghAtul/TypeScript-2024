c0b6f06fa15dca1b508e80f2d8392b50b9aea01e
Atulsrj <atulsinghsrj@gmail.com>
2024-01-10 14:21:12 +0530

    Typescript Done

--- purets/src/index.ts
+++ purets/src/index.ts
@@ -204,3 +204,195 @@ function identityFour<T>(val: T): T {
   return val;
 }
 identityFour("score");
+
+interface Bootle {
+  brand: string;
+  type: number;
+}
+
+identityFour<Bootle>({ brand: "g", type: 5 });
+
+//! 03:47:41 - 03:55:50 - Generics in Array and Arrow functions in Typescript
+
+function getSearchproducts<T>(products: T[]): T {
+  // do some database opertaions
+  const myIndex = 3;
+  return products[myIndex];
+}
+
+const getMoreSearchproducts = <T>(products: T[]): T => {
+  const myIndex = 3;
+  return products[myIndex];
+};
+
+//! 03:55:50 - 04:06:59 - Generic Classes in Typescript
+
+function anotherFunction<T, U>(valOne: T, valTwo: U): object {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+anotherFunction(3, 6);
+anotherFunction(3, "6");
+
+function anotherFunctionTwo<T, U extends string>(valOne: T, valTwo: U): object {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+anotherFunctionTwo(6, "7");
+// anotherFunctionTwo(6, 7);
+
+interface Database {
+  connection: string;
+  username: string;
+  password: string;
+}
+
+function anotherFunctionThree<T, U extends Database>(valOne: T, valTwo: U) {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+anotherFunctionThree(6, {
+  connection: "",
+  username: "",
+  password: "string;",
+});
+
+//! Using Class Type in generics (<T>)
+
+interface Quiz {
+  name: string;
+  type: string;
+}
+
+interface Course {
+  name: string;
+  author: string;
+  subject: string;
+}
+
+class Sellable<T> {
+  public cart: T[] = [];
+
+  addToCart(product: T) {
+    this.cart.push(product);
+  }
+}
+
+//! 04:06:59 - 04:16:47 - Type Narrowing in typescript
+
+function detectType(val: number | string) {
+  if (typeof val === "string") {
+    return val.toLowerCase();
+  }
+  return val + 3;
+}
+
+function provideId(id: string | null) {
+  if (!id) {
+    console.log("Please provide ID");
+    return;
+  }
+  id.toLowerCase();
+}
+
+//! 04:16:47 - 04:22:00 - The in operator narrowing
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface Admin {
+  name: string;
+  email: string;
+  isAdmin: boolean;
+}
+
+function isAdminAccount(account: User | Admin) {
+  if ("isAdmin" in account) {
+    return account.isAdmin;
+  }
+}
+
+//! 04:22:00 - 04:31:18 - Instanceof and Type Predicates
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+//! Using type Predicates
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "Fish Food";
+  } else {
+    pet;
+    return "bird food";
+  }
+}
+
+//! 04:31:18 - 04:42:37 - Discriminated Union and Exhaustiveness Checking with never
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  side: number;
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  width: number;
+  length: number;
+}
+
+//^ Discriminated Union check
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  // return shape.side * shape.side;
+}
+
+//^ Exhaustiveness Checking with never
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+
+    case "square":
+      return shape.side * shape.side;
+
+    case "rectangle":
+      return shape.width * shape.length;
+
+    default:
+      const _exhaustiveCheck: never = shape;
+      return _exhaustiveCheck;
+  }
+}
+
+//! ///////////// Narrowing Done /////////////////////
+//! ///////////// Typescript Done /////////////////////
